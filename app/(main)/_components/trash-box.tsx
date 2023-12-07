7461fa5dddb6c0d99ec8294e4e09c39305eebc07
Alohadancemeow <nawongsak@gmail.com>
2023-12-07 14:56:59 +0700

    🍀 trash box /remove-restore

---
+++ app/(main)/_components/trash-box.tsx
@@ -0,0 +1,113 @@
+"use client";
+
+import { useState } from "react";
+import { useParams, useRouter } from "next/navigation";
+import { useQuery, useMutation } from "convex/react";
+import { Search, Trash, Undo } from "lucide-react";
+import { toast } from "sonner";
+
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { Spinner } from "@/components/spinner";
+import { Input } from "@/components/ui/input";
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+
+export const TrashBox = () => {
+  const router = useRouter();
+  const params = useParams();
+  const documents = useQuery(api.documents.getTrash);
+  const restore = useMutation(api.documents.restore);
+  const remove = useMutation(api.documents.remove);
+
+  const [search, setSearch] = useState("");
+
+  const filteredDocuments = documents?.filter((document) => {
+    return document.title.toLowerCase().includes(search.toLowerCase());
+  });
+
+  const onClick = (documentId: string) => {
+    router.push(`/documents/${documentId}`);
+  };
+
+  const onRestore = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+    documentId: Id<"documents">
+  ) => {
+    event.stopPropagation();
+    const promise = restore({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Restoring note...",
+      success: "Note restored!",
+      error: " Failed to restore note.",
+    });
+  };
+
+  const onRemove = (documentId: Id<"documents">) => {
+    const promise = remove({ id: documentId });
+
+    toast.promise(promise, {
+      loading: "Deleting note...",
+      success: "Note deleted!",
+      error: " Failed to delete note.",
+    });
+
+    if (params.documentId === documentId) {
+      router.push("/documents");
+    }
+  };
+
+  if (documents === undefined) {
+    return (
+      <div className="flex items-center justify-center h-full p-4">
+        <Spinner size="lg" />
+      </div>
+    );
+  }
+
+  return (
+    <div className="text-sm">
+      <div className="flex items-center p-2 gap-x-1">
+        <Search className="w-4 h-4" />
+        <Input
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          className="px-2 h-7 focus-visible:ring-transparent bg-secondary"
+          placeholder="Filter by page title..."
+        />
+      </div>
+      <div className="px-1 pb-1 mt-2">
+        <p className="hidden pb-2 text-xs text-center last:block text-muted-foreground">
+          No documents found.
+        </p>
+        {filteredDocuments?.map((document) => (
+          <div
+            key={document._id}
+            role="button"
+            onClick={() => onClick(document._id)}
+            className="flex items-center justify-between w-full text-sm rounded-sm hover:bg-primary/5 text-primary"
+          >
+            <span className="pl-2 truncate">{document.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, document._id)}
+                role="button"
+                className="p-2 rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-600"
+              >
+                <Undo className="w-4 h-4 text-muted-foreground" />
+              </div>
+              <ConfirmModal onConfirm={() => onRemove(document._id)}>
+                <div
+                  role="button"
+                  className="p-2 rounded-sm hover:bg-neutral-200 dark:hover:bg-neutral-600"
+                >
+                  <Trash className="w-4 h-4 text-muted-foreground" />
+                </div>
+              </ConfirmModal>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
